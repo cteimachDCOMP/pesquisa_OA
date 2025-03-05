f79ce1ef9c960e2a0914b0fee17aea83f2ee7fc0
Cauan Teixeira Machado <cauan.machado@dcomp.ufs.br>
2025-03-05 13:55:44 -0300

    Create multiplex_5_dec7.ino

---
+++ Instrumentalização/manipulacao_dec7/multiplex_5_dec7/multiplex_5_dec7.ino
@@ -0,0 +1,144 @@
+// Multiplexação de 5 Displays de 7 segmentos
+#include <TimerOne.h>
+
+#define DISP_UNIDADE 10
+#define DISP_DEZENA 11
+#define DISP_CENTENA 12
+#define DISP_MILHAR A0
+#define DISP_DEZMILHAR A1
+
+int num = 0;
+int valor_unidade = 0, valor_dezena = 0, valor_centena = 0, valor_milhar = 0, valor_dezenaMilhar = 0;
+int alterna = 1;
+int disposicao_pinos[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+
+int digitos[16][8] = {
+  { 1, 1, 1, 1, 1, 1, 0, 0 },  // 0
+  { 0, 1, 1, 0, 0, 0, 0, 0 },  // 1
+  { 1, 1, 0, 1, 1, 0, 1, 0 },  // 2
+  { 1, 1, 1, 1, 0, 0, 1, 0 },  // 3
+  { 0, 1, 1, 0, 0, 1, 1, 0 },  // 4
+  { 1, 0, 1, 1, 0, 1, 1, 0 },  // 5
+  { 1, 0, 1, 1, 1, 1, 1, 0 },  // 6
+  { 1, 1, 1, 0, 0, 0, 0, 0 },  // 7
+  { 1, 1, 1, 1, 1, 1, 1, 0 },  // 8
+  { 1, 1, 1, 0, 0, 1, 1, 0 },  // 9
+  { 1, 1, 1, 0, 1, 1, 1, 0 },  // A
+  { 0, 0, 1, 1, 1, 1, 1, 0 },  // B
+  { 1, 0, 0, 1, 1, 1, 0, 0 },  // C
+  { 0, 1, 1, 1, 1, 0, 1, 0 },  // D
+  { 1, 0, 0, 1, 1, 1, 1, 0 },  // E
+  { 1, 0, 0, 0, 1, 1, 1, 0 }   // F
+};
+
+void imprimeNumero(int n) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(disposicao_pinos[i], digitos[n][i]);
+  }
+}
+
+void trocarDisplay() {
+  digitalWrite(DISP_UNIDADE, LOW);
+  digitalWrite(DISP_DEZENA, LOW);
+  digitalWrite(DISP_CENTENA, LOW);
+  digitalWrite(DISP_MILHAR, LOW);
+  digitalWrite(DISP_DEZMILHAR, LOW);
+
+  delayMicroseconds(100);
+
+  switch (alterna) {
+    case 1:
+      digitalWrite(DISP_UNIDADE, LOW);
+      digitalWrite(DISP_DEZENA, LOW);
+      digitalWrite(DISP_CENTENA, LOW);
+      digitalWrite(DISP_MILHAR, LOW);
+      digitalWrite(DISP_DEZMILHAR, LOW);
+      
+      imprimeNumero(valor_unidade);
+      digitalWrite(DISP_UNIDADE, HIGH);
+      break;
+
+    case 2:
+      digitalWrite(DISP_UNIDADE, LOW);
+      digitalWrite(DISP_DEZENA, LOW);
+      digitalWrite(DISP_CENTENA, LOW);
+      digitalWrite(DISP_MILHAR, LOW);
+      digitalWrite(DISP_DEZMILHAR, LOW);
+      
+      imprimeNumero(valor_dezena);
+      digitalWrite(DISP_DEZENA, HIGH);
+      break;
+
+    case 3:
+      digitalWrite(DISP_UNIDADE, LOW);
+      digitalWrite(DISP_DEZENA, LOW);
+      digitalWrite(DISP_CENTENA, LOW);
+      digitalWrite(DISP_MILHAR, LOW);
+      digitalWrite(DISP_DEZMILHAR, LOW);
+      
+      imprimeNumero(valor_centena);
+      digitalWrite(DISP_CENTENA, HIGH);
+      break;
+
+    case 4:
+      digitalWrite(DISP_UNIDADE, LOW);
+      digitalWrite(DISP_DEZENA, LOW);
+      digitalWrite(DISP_CENTENA, LOW);
+      digitalWrite(DISP_MILHAR, LOW);
+      digitalWrite(DISP_DEZMILHAR, LOW);
+      
+      imprimeNumero(valor_milhar);
+      digitalWrite(DISP_MILHAR, HIGH);
+      break;
+
+    case 5:
+      digitalWrite(DISP_UNIDADE, LOW);
+      digitalWrite(DISP_DEZENA, LOW);
+      digitalWrite(DISP_CENTENA, LOW);
+      digitalWrite(DISP_MILHAR, LOW);
+      digitalWrite(DISP_DEZMILHAR, LOW);
+      
+      imprimeNumero(valor_dezenaMilhar);
+      digitalWrite(DISP_DEZMILHAR, HIGH);
+      break;
+  }
+
+  alterna++;
+  if (alterna > 5) {
+    alterna = 1;
+  }
+}
+
+void setup() {
+  pinMode(DISP_UNIDADE, OUTPUT);
+  pinMode(DISP_DEZENA, OUTPUT);
+  pinMode(DISP_CENTENA, OUTPUT);
+  pinMode(DISP_MILHAR, OUTPUT);
+  pinMode(DISP_DEZMILHAR, OUTPUT);
+
+  for (int i = 0; i < 8; i++) {
+    pinMode(disposicao_pinos[i], OUTPUT);
+  }
+
+  imprimeNumero(2);
+
+  Timer1.initialize(5000);
+  //Timer1.attachInterrupt(trocarDisplay);
+}
+
+void loop() {
+  valor_unidade = num % 16;
+  valor_dezena = (num / 16) % 16;
+  valor_centena = (num / 256) % 16;
+  valor_milhar = (num / 4096) % 16;
+  valor_dezenaMilhar = (num / 65536) % 16;
+
+  num++;
+
+  if (num > 1048575) {
+    num = 0;
+  }
+
+  delay(1000);
+}
+
